58c18c520fcb533b9bdf96c9d2793d28d09b3409
Robin Allen <r@foon.uk>
2016-03-03 14:10:04 +0000

    Specify a stable release.
    Homebrew does not let us depend on neovim@HEAD, so it no longer makes
    sense for this formula to be HEAD-only, as our HEAD now depends on
    Neovim features not present in their stable releases.

--- neovim-dot-app.rb
+++ neovim-dot-app.rb
@@ -1,6 +1,8 @@
 class NeovimDotApp < Formula
   homepage "https://github.com/rogual/neovim-dot-app"
   head "https://github.com/rogual/neovim-dot-app.git"
+  url "https://github.com/rogual/neovim-dot-app/archive/0.1.2.tar.gz"
+  sha256 "88ad4af6bdd8f0cd008b3ff065fca53ef7870c388ba62a060cb2bdcb19709801"
 
   depends_on "scons"     => :build
   depends_on "pkgconfig" => :build
